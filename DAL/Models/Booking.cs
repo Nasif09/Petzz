07526227a6aaee530183b936b32d54784841d542
unknown <mdnasifurahman@gmail.com>
2023-12-27 00:44:35 +0600

    Petzz

---
+++ DAL/Models/Booking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Booking
+    {
+        public int BookingID { get; set; }
+        public int UID { get; set; }
+        public int RoomID { get; set; }   
+        public int BranchID { get; set; }
+    }
+}
---
+++ DAL/Models/Branch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Branch
+    {
+        public int BranchID { get; set; }
+        public string Brancename { get; set; }
+        public string Location { get; set; }
+        public int? UID { get; set; }
+    }
+}
